1c16fb67d19bdb595db55fd10a5c12b92128cada
moateff <mohamed.atef.yousef46@gmail.com>
2025-12-08 11:19:39 +0200

    Commit DS

--- DS/lab1/quickSort.cpp
+++ DS/lab1/quickSort.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
 using namespace std;
 
+void swap(int& x, int& y) {
+    int temp = x;
+    x = y;
+    y = temp;
+}
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+
+    for (int j = low; j < high; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+
+    swap(arr[i + 1], arr[high]);
+    return i + 1;
+}
+
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+void quickSort(int arr[], int n) {
+    quickSort(arr, 0, n - 1);
+}
 
 void printArray(int arr[], int n) {
     for (int i = 0; i < n; i++) {
@@ -13,7 +45,7 @@ int main() {
     int arr[] = {64, 34, 25, 12, 22, 11, 90};
     int n = sizeof(arr) / sizeof(arr[0]);
 
-    // quickSort(arr, n);
+    quickSort(arr, n);
     cout << "Sorted array: ";
     printArray(arr, n);
 
