014a299fbb187ba38da4cb5558ebc234a5bee5a1
moateff <mohamed.atef.yousef46@gmail.com>
2026-04-05 00:09:09 +0200

    Commit All

--- C#/lab10/EmployeeLayOffEventArgs.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace Task
-{
-    public class EmployeeLayOffEventArgs : EventArgs
-    {
-        public LayOffCause Cause { get; set; }
-    }
-}
--- C#/lab7/Src/ExamStartedHandler.cs
+++
@@ -1,6 +0,0 @@
-using System;
-
-namespace Examination.Src
-{
-    public delegate void ExamStartedHandler(object sender, ExamEventArgs e);
-}
